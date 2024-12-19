ca1c49ad28f11648b0b51acc9a3d5b65b1c5765b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-20 00:53:11 +0200

    Update exercise1.hs

--- Seminars/Seminar. 09/exercise1.hs
+++ Seminars/Seminar. 09/exercise1.hs
@@ -2,6 +2,6 @@ module Fact (fact) where
 
 fact :: Int -> Int
 fact n
-  | n < 0     = error "Факториелът не е дефиниран за отрицателни числа"
+  | n < 0     = error 
   | n == 0    = 1
   | otherwise = n * fact (n - 1)
